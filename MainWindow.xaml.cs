71ea9d6c9994616bef90a7106b04af39c23d2056
Peachywind <89639220+Peachywind@users.noreply.github.com>
2023-06-21 03:19:18 +0300

    a
    a

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Net.Http;
+
+namespace API
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           /* LoadProvincesAsync();*/
+        }
+
+       /* private void LoadProvincesAsync()
+        {
+            var modelWrapper = new ModelDataGridWrapper<Root>(Location=>Location);
+            var result = modelWrapper.LoadProvincesAsync();
+           
+
+
+        }*/
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ApiHelper.Get(Sity.Text);
+
+        }
+    }
+}
---
+++ Model/Helper/ApiHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows;
+
+internal class ApiHelper
+{
+    public static string Url = "http://api.weatherapi.com/v1/forecast.json?key=0685a95f84dc402996561002231606&";
+
+    public static void Get(string Sity)
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.GetAsync(Url + "q=" + Sity).Result;
+            string responseContent = response.Content.ReadAsStringAsync().Result;
+
+            // Десериализация содержимого ответа для извлечения данных о погодных условиях
+            var weatherData = JsonConvert.DeserializeObject<Root>(responseContent);
+            Forecast location = weatherData.Forecast;
+
+            // Отображение данных о погодных условиях в MessageBox
+            var message = $"Погодные условия: {location.forecastday[0].hour[4].temp_c}";
+            MessageBox.Show(message);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+}
